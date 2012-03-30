8341de77fef1693fadd05aabb9ec05e40a09b67e
Igor Davydenko <playpauseandstop@gmail.com>
2012-03-30 14:42:39 +0300

    Little modification to ``wsgi`` module.

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-# Code from http://flask.pocoo.org/snippets/48/
-import sys
-sys.path.append('/home/dotcloud/current')
+import os
+
+# Add root dotcloud folder if possible
+if os.path.isdir('/home/dotcloud/current'):
+    import sys
+    sys.path.append('/home/dotcloud/current')
+
 from watracest.app import app as application
