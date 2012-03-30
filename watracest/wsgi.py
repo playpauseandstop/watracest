3c161025587f9f923a13ed180aa73a650e00a43f
Igor Davydenko <playpauseandstop@gmail.com>
2012-03-30 14:27:50 +0300

    Update placement of wsgi Python module.

--- watracest/wsgi.py
+++
@@ -1,6 +0,0 @@
-from watracest.app import app
-
-
-# For DotCloud we need to provide ``application`` var in ``<approot>.wsgi``
-# Python module
-application = app
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+# Code from http://flask.pocoo.org/snippets/48/
+import sys
+sys.path.append('/home/dotcloud/current')
+from watracest.app import app as application
